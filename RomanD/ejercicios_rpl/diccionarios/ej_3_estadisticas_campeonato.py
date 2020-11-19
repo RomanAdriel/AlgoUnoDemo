3996d4951dbac936b9c3dbd7ce2cc827abb265c7
Roman Diaz <roman.diaz09@gmail.com>
2020-11-19 00:25:27 -0300

    EJ 4 RPL Diccionarios

---
+++ RomanD/ejercicios_rpl/diccionarios/ej_3_estadisticas_campeonato.py
@@ -0,0 +1,48 @@
+"""El siguiente ejercicio nos pide extraer estadísticas para un campeonato de fútbol finalizado a partir de ciertos
+datos que se encuentran cargados en un diccionario importado desde un archivo secundario.
+
+El diccionario tiene como clave el nombre del equipo de fútbol y como valor asociado una tupla de largo 5, que contiene
+información sobre el desempeño de los equipos en el campeonato. En la primera posición tiene la cantidad de partidos
+ganados (vale por 3 puntos); en la segunda posición la cantidad de partidos empatados (vale por 1 punto); en la tercera
+posición la cantidad de partidos perdidos (vale por 0 puntos); en la cuarta posición la cantidad de goles realizados;
+y en la quinta posición la cantidad de goles recibidos.
+
+A partir de importar este diccionario, se pide construir un programa que calcule las siguientes estadísticas:
+
+    El equipo que salió campeón, con su respectiva cantidad de puntos.
+    El equipo que desciende (último), con su respectiva cantidad de puntos.
+    El partido que más partidos empatados tuvo, con su respectiva cantidad.
+    El equipo que tiene el ratio goles realizados sobre goles recibidos más alto de todos (goles realizados / goles
+    recibidos).
+
+La salida debe tener el siguiente formato:
+
+El equipo campeon es Boca con 26 puntos.
+El equipo que desciende es San Lorenzo con 4 puntos.
+El equipo que mas partidos empato es River con 6 partidos.
+El equipo con mejor proporcion goleadora es Huracan con 2.6.
+
+Nota importante: Está claro que conociendo el diccionario pueden hacer las cuentas a mano y printear las respuestas.
+El objetivo del ejercicio es que hagan los cálculos. Nuevamente, confiamos en su honestidad y responsabilidad para
+aprovechar el ejercicio para practicar."""
+
+campeonato = {
+    "Boca": (2, 3, 4, 10, 10),
+    "River": (3, 4, 2, 12, 16),
+    "Central": (7, 1, 1, 23, 21),
+    "Racing": (5, 2, 2, 15, 8),
+    "Independiente": (4, 3, 2, 7, 8),
+    "San Lorenzo": (1, 8, 0, 5, 9),
+    "Godoy Cruz": (3, 3, 1, 3, 3),
+    "Estudiantes": (1, 1, 7, 9, 9),
+    "Huracan": (3, 2, 4, 4, 6),
+    "Gimnasia LP": (0, 2, 7, 1, 26)
+}
+
+def calcular_puntos(tabla):
+
+
+def calcular_dif_gol(tabla):
+
+
+
--- RomanD/ejercicios_rpl/diccionarios/ej_4_tipo_parcial.py
+++ RomanD/ejercicios_rpl/diccionarios/ej_4_tipo_parcial.py
@@ -43,3 +43,63 @@ En la localidad de Chascomus hay 10 personas en edad laboral y 6 trabajando.
 La tasa de desocupacion en Chascomus es 40.0%.
 La tasa de desocupacion en Lanus es 35.0%.
 La tasa de desocupacion en CABA es 25.0%."""
+
+
+def ingreso_datos(mensaje):
+    valor_ingresado = input(mensaje)
+
+    return valor_ingresado
+
+
+def carga_datos():
+    tabla_localidades = {}
+    corte = "s"
+
+    while corte != "n":
+
+        localidad = ingreso_datos("Ingrese localidad: ")
+        trabajadores = int(ingreso_datos("Ingrese la cantidad de personas que pueden trabajar: "))
+        empleados = int(ingreso_datos("Ingrese la cantidad de empleados: "))
+
+        if localidad not in tabla_localidades:
+            tabla_localidades[localidad] = [trabajadores, empleados]
+
+        else:
+            tabla_localidades[localidad][0] += trabajadores
+            tabla_localidades[localidad][1] += empleados
+
+        corte = ingreso_datos("Desea seguir ingresando?(s/n): ")
+
+    return tabla_localidades
+
+
+def calcular_desocupacion(tabla):
+    lista = []
+
+    for registro in tabla.items():
+        desocupacion = registro[0], (1 - registro[1][1] / registro[1][0]) * 100
+        lista.append(desocupacion)
+
+    lista.sort(key=lambda tupla: tupla[1], reverse=True)
+
+    return lista
+
+
+def imprimir_datos(tabla, lista):
+    for registro in tabla.items():
+        print(
+            "En la localidad de {localidad} hay {trabajadores} personas en edad laboral y {empleados} trabajando.".format(
+                localidad=registro[0], trabajadores=registro[1][0], empleados=registro[1][1]))
+
+    for elementos in lista:
+        print("La tasa de desocupacion en {localidad} es {desocupacion}%.".format(localidad=elementos[0],
+                                                                                  desocupacion=elementos[1]))
+
+
+def main():
+    tabla_localidades = carga_datos()
+    cantidad_desocupados = calcular_desocupacion(tabla_localidades)
+    imprimir_datos(tabla_localidades, cantidad_desocupados)
+
+
+main()
