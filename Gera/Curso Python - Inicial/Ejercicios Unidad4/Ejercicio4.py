ccf6f492ed0fd41891b682c7040f6f00df24fc2f
AgustinCabrera85 <68361624+AgustinCabrera85@users.noreply.github.com>
2021-06-12 18:45:37 -0300

    Ejercicio 4 unidad 4 UTN

---
+++ Gera/Curso Python - Inicial/Ejercicios Unidad4/Ejercicio4.py
@@ -0,0 +1,44 @@
+"""Escriba un programa que permita tomar una palabra retorne las vocales que contenga junto con la cantidad de veces
+que se repite. """
+
+
+def ingreso_palabra():
+    palabra = input("Ingrese palabra: ")
+    return palabra
+
+
+def string_to_list(parametro):
+    lista_convertida = list(parametro)
+    return lista_convertida
+
+
+def count_vocales(word):
+    palabra = word
+    cantidad_a = palabra.count('a')
+    cantidad_A = palabra.count('A')
+    cantidad_e = palabra.count('e')
+    cantidad_E = palabra.count('E')
+    cantidad_i = palabra.count('i')
+    cantidad_I = palabra.count('I')
+    cantidad_o = palabra.count('o')
+    cantidad_O = palabra.count('O')
+    cantidad_u = palabra.count('u')
+    cantidad_U = palabra.count('U')
+    return {'A': cantidad_a+cantidad_A,
+            'E': cantidad_e+cantidad_E,
+            'I': cantidad_i+cantidad_I,
+            'O': cantidad_o+cantidad_O,
+            'U': cantidad_u+cantidad_U}
+
+
+
+def imprimir_resultado(mensaje, resultado):
+
+    print(mensaje, resultado)
+
+
+def main():
+    imprimir_resultado("Las vocales se repiten esta cantidad de veces: ", count_vocales(string_to_list(ingreso_palabra())))
+
+
+__init__: main()
