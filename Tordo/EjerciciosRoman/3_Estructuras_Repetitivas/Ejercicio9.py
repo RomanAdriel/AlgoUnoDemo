8ca576cd97e809d1d20a9086583625816e97558b
unknown <jmanuel.pat@gmail.com>
2021-06-16 23:10:34 -0300

    Ejercicios de estructuras repetitivas

--- Tordo/EjerciciosRoman/3_Estructuras_Repetitivas/Ejercicio9.py
+++ Tordo/EjerciciosRoman/3_Estructuras_Repetitivas/Ejercicio9.py
@@ -1,32 +1,27 @@
 """Dada una serie de números enteros terminada en cero, imprimir los tres mayores. """
 
-# lista = [1, 6, 4, 8, 7, 5, -1, 0]
-# mayor = 0
-#
-# for i in lista:
-#     if mayor == 0:
-#
-#         mayor = i
-#
-#     elif i > mayor:
-#         mayor = i
-#
-# print("El numero mayor es: " + str(mayor))
+"""A mejorar: """
 
 
 numero = int(input("Ingrese valor (0 para terminar): "))
-oro = numero
-plata = numero
-bronce = numero
+oro = 0
+plata = 0
+bronce = 0
 
 while numero != 0:
-    numero = int(input("Ingrese valor (0 para terminar): "))
+
     if numero > oro:
+        bronce = plata
+        plata = oro
         oro = numero
-    elif oro > numero > plata:
+
+    elif oro >= numero > plata:
         bronce = plata
         plata = numero
-    elif plata > numero > bronce:
+
+    elif plata >= numero > bronce:
         bronce = numero
+
+    numero = int(input("Ingrese valor (0 para terminar): "))
 
 print(oro, plata, bronce)
---
+++ Tordo/EjerciciosRoman/3_Estructuras_Repetitivas/Ejercicio9Bis.py
@@ -0,0 +1,3 @@
+"""Dada una serie de números enteros terminada en cero, imprimir los tres mayores. """
+"""Adicionalmente se solicita que no haya numeros duplicados y que en caso de ingresar menos de 3 números solo considere
+ los numeros ingresados"""
