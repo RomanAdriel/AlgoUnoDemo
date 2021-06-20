0cd8408ae6d102cb4e0584b1b92235c8b4dcf261
AgustinCabrera85 <68361624+AgustinCabrera85@users.noreply.github.com>
2021-06-19 22:31:12 -0300

    Ejercicio 8 de funciones refactorizado

--- Gera/Ejercicios UBA/Funciones/Ejercicio8.py
+++ Gera/Ejercicios UBA/Funciones/Ejercicio8.py
@@ -6,41 +6,45 @@ igual a cero. No se deben guardar los datos.
 
 
 def ingresar_datos():
-    recaudacion = int(input("ingrese recaudacion en $"))
-    costo = int(input("ingrese costo en $ "))
+    recaudacion = float(input("ingrese recaudacion en $"))
+    costo = float(input("ingrese costo en $ "))
     mes = int(input("ingrese numero de mes: "))
 
     return mes, recaudacion, costo
 
 
-def calcular_mes_ganancia(datos):
-    mes, recaudacion, costo = datos
+def calcular_mes_ganancia(datos_ingresados):
+    mes, recaudacion, costo = datos_ingresados
     ganancia = recaudacion - costo
 
     return mes, ganancia
 
 
-def comparar_ganancias(dato1, dato2):
-    if dato1[1] > dato2[1]:
-        mayor_ganancia = dato1
+def comparar_ganancias(ganancia_mensual1, ganancia_mensual2):
+    indice_ganancia = 1
+    if ganancia_mensual1[indice_ganancia] > ganancia_mensual2[indice_ganancia]:
+        mayor_ganancia = ganancia_mensual1
     else:
-        mayor_ganancia = dato2
+        mayor_ganancia = ganancia_mensual2
     return mayor_ganancia
 
 
 def mes_maximo():
-    valor1 = calcular_mes_ganancia(ingresar_datos())
-    if valor1[0] != 0:
-        valor2 = calcular_mes_ganancia(ingresar_datos())
-        while valor2[0] != 0:
-            valor1 = comparar_ganancias(valor1, valor2)
-            valor2 = calcular_mes_ganancia(ingresar_datos())
-        return valor1
+    indice_mes = 0
+    ganancia_max = calcular_mes_ganancia(ingresar_datos())
+    if ganancia_max[indice_mes] != 0:
+        comparar_ganancia = calcular_mes_ganancia(ingresar_datos())
+        while comparar_ganancia[indice_mes] != 0:
+            valor1 = comparar_ganancias(ganancia_max, comparar_ganancia)
+            comparar_ganancia = calcular_mes_ganancia(ingresar_datos())
+    return ganancia_max
 
 
 def imprimir_resultado(datos_mes_maximo):
+    indice_mes = 0
+    indice_ganancia = 1
     if datos_mes_maximo:
-        print("El mes", datos_mes_maximo[0], "tuvo una ganancia de $", datos_mes_maximo[1], "y fue el máximo")
+        print("El mes", datos_mes_maximo[indice_mes], "tuvo una ganancia de $", datos_mes_maximo[indice_ganancia], "y fue el máximo")
     else:
         print("Datos insuficientes")
 
